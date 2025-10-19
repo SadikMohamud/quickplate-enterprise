a29594591827670352a06cd7720c2b860e2397d5
Sadik Mohamud <msadiknur@gmail.com>
2025-10-19 13:53:17 +0100

    Update supabase.js

--- quickplate-enterprise/packages/restaurant-admin/lib/supabase.js
+++ quickplate-enterprise/packages/restaurant-admin/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.https://ojsbaylycmkhisanhwxz.supabase.co,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qc2JheWx5Y21raGlzYW5od3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzcwNDksImV4cCI6MjA3NjQxMzA0OX0.pSOgeVV8K4jIJ6l4vmd3StTvXQ9xp9zwMlGmn8ZYEVY
 );
